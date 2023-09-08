a76919f45968d798fde0a3e2fa4b59644c790a73
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-09-08 04:25:53 +0000

    Added few more pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //Pages
 import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticlesList from "./pages/ArticlesList";
+import Article from "./pages/Article";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/articles-list" element={<ArticlesList />}></Route>
+          <Route path="/article" element={<Article />}></Route>
         </Routes>
       </div>
     </Router>
---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+const About = (props) => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        About
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Deserunt cupidatat commodo excepteur quis proident consequat aute ut.
+        Sit minim officia fugiat reprehenderit enim Lorem occaecat culpa magna
+        exercitation exercitation anim ad ea. Ut tempor nisi laborum laborum qui
+        duis laborum nostrud duis pariatur sit. Cillum culpa mollit pariatur
+        voluptate deserunt voluptate officia sit veniam Lorem sunt eu. Consequat
+        cupidatat excepteur amet officia cillum. Culpa ex proident sit do cillum
+        ipsum consectetur do. Lorem nulla dolor ad magna magna.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Culpa fugiat eiusmod adipisicing qui elit consectetur eiusmod. Pariatur
+        elit voluptate aliqua consectetur. Mollit et laboris non officia fugiat
+        voluptate qui magna nulla non nostrud duis fugiat.Cillum culpa mollit
+        pariatur voluptate deserunt voluptate officia sit veniam Lorem sunt eu.
+        Consequat cupidatat excepteur amet officia cillum. Culpa ex proident sit
+        do cillum ipsum consectetur do. Lorem nulla dolor ad magna magna.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Consequat amet nisi exercitation nostrud. Eu aute velit ut ex esse magna
+        aute cupidatat. Et deserunt dolore incididunt amet id laborum voluptate
+        ut nisi adipisicing. Consequat sit occaecat exercitation eu exercitation
+        quis est ullamco veniam reprehenderit veniam veniam reprehenderit.
+        Veniam mollit officia velit enim sint et id veniam aute minim sunt
+        laborum.
+      </p>
+    </div>
+  )
+};
+
+export default About;
