7dba83978316d81554f90a8f2061716b85a21dae
Kelvin Botchway <Kelvinjay1000@gmail.com>
2023-09-08 18:58:31 +0000

    The about page done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import About from "./pages/About";
 import ArticlesList from "./pages/ArticlesList";
 import Article from "./pages/Article";
 
+//components
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/articles-list" element={<ArticlesList />}></Route>
-          <Route path="/article" element={<Article />}></Route>
+          <Route path="/article/:name" element={<Article />}></Route>
         </Routes>
       </div>
     </Router>
